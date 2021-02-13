0c8fe1c0aa44b1509a9575018e1dac2442b661f9
berat422 <75537316+berat422@users.noreply.github.com>
2021-02-13 21:48:44 +0100

    Update Mapper.php
    mapping

--- Mapper.php
+++ Mapper.php
@@ -1,1 +1,59 @@
-//
+<?php
+
+require_once "DataBaseConfig.php";
+
+class Mapper extends DataBaseConfig
+
+{
+
+    private $connection;
+    private $query;
+
+    public function __construct()
+    {
+        $this->connection = $this->getConnection();
+    }
+
+   
+    
+    public function getUserByUsername($username)
+    {
+        $this->query = "select * from user where name=:name";
+        $statement = $this->connection->prepare($this->query);
+        $statement->bindParam(":name", $username);
+        $statement->execute();
+        return $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        
+    }
+
+    public function getAllUsers()
+    {
+        $this->query = "select * from user";
+        $statement = $this->connection->prepare($this->query);
+        $statement->execute();
+        return $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+       
+    }
+
+    public function insertUser(\Simpleuser $user)
+    {
+        $this->query = "insert into user (name,email,password, role) values (:name,:mail,:password,:role)";
+        $statement = $this->connection->prepare($this->query);
+        $username = $user->getUserName();
+        $email=$user->getEmail();
+        $password = password_hash($user->getPassword(), PASSWORD_BCRYPT);
+        $role = $user->getRole();
+        $statement->bindParam(":name", $username);
+        $statement->bindParam(":mail", $email);
+        $statement->bindParam(":password", $password);
+       $statement->bindParam(":role", $role);
+        $statement->execute();
+    }
+
+    
+}
+
+
+
+
+?>
