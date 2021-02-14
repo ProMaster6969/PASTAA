1d2698d26293fbbb20662e1bf200e09405ca0609
berat422 <75537316+berat422@users.noreply.github.com>
2021-02-14 11:36:22 +0100

    Update LoginVerify.php
    register//

--- LoginVerify.php
+++ LoginVerify.php
@@ -1,1 +1,36 @@
-//
+<?
+class RegisterLogic
+{
+    private $username = "";
+    private $useremail="";
+    private $password = "";
+   
+
+    public function __construct($data)
+    {
+        $this->username = $data['register-name'];
+        $this->useremail=$data['email'];
+        $this->password = $data['register-password'];
+       
+        
+    }
+    public function verifyData()
+    {
+        
+        echo $this->username;
+        echo $this->useremail;
+        echo $this->password;
+        if ($this->variablesNotDefinedWell($this->username,$this->useremail, $this->password)) {
+            header("Location:Register.php");
+        
+        } else if ($this->usernameandemailandPasswordCorrect($this->username,$this->useremail ,$this->password)) {
+            
+             header('Location:Projekti.php');
+        } else {
+             header("Location:Register.php");
+           
+        }
+    }
+    //
+
+    ?>
