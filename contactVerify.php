50249df796e6b7f5bd87fb15238b74f161a47afe
ProMaster6969 <75331370+ProMaster6969@users.noreply.github.com>
2021-02-14 15:47:29 +0100

    Update contactVerify.php
    e verifikojm kontaktin

--- contactVerify.php
+++ contactVerify.php
@@ -1,1 +1,19 @@
-//
+<?php
+include_once 'Mapper.php';
+if (isset($_POST['talk-btn'])){
+  
+  $contact=new contactVerify($_POST);
+   if( !$contact->verifyDB()){
+     
+       header('Location:ContactUs.php');
+   }
+   else{
+     $array=$contact->verifyDB();
+     $mapper=new Mapper();
+     $mapper->InsertintoContact($array[0],$array[1],$array[2]);
+     header('Location:Projekti.php');
+     
+   }
+}
+
+?>
