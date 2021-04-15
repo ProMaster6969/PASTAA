47dbbf28789430f39929ffe186da48175cc19510
ProMaster6969 <75331370+ProMaster6969@users.noreply.github.com>
2021-04-15 21:47:50 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,30 +1,32 @@
-<footer>
-        <div class="container-fluid footer">
-          <div class="container col-sm-12 col-md-11 col-lg-7 pt-5">
-            <div class="row">
+<?php
+require_once "Mapper.php";
+$mapper=new Mapper();
+echo "<footer>".
+        "<div class=\"container-fluid footer\">".
+          "<div class=\"container col-sm-12 col-md-11 col-lg-7 pt-5\">"
+            ."<div class=\"row\">
 
-               <div class="col-md-4 col-lg-4 pb-4">
-                  <img src="Photos/logo.png" class="pb-3"><br>
-                  <i class="fa fa-facebook"></i>
-                  <i class="fa fa-twitter"></i>
-                  <i class="fa fa-youtube"></i>
+               <div class=\"col-md-4 col-lg-4 pb-4\">
+                  <img src=\"Photos/logo.png\" class=\"pb-3\"><br>
+                  <i class=\"fa fa-facebook\"></i>
+                  <i class=\"fa fa-twitter\"></i>
+                  <i class=\"fa fa-youtube\"></i>
                </div>
 
-               <div class="col-md-4 col-lg-4">
-                  <h1>Who are we?</h1>
-                  <p> Yep, we’re the place with the delicious pasta  - Oh, and our main ingredient is love!.</p>
-               </div>
+               <div class=\"col-md-4 col-lg-4\">";
+                 echo $mapper->getFooter(1)['text'];
+                 echo $mapper->getFooter(2)['text'];
+               echo"</div>
 
-               <div class="col-md-4 col-lg-4 ndryshe pb-5">
-                  <h1>Contact Info</h1>
-
-                  <ul>
-                    <li><i class="fa fa-clock-o pr-2"></i>E Hëne – E Shtune 8am – 24pm (Extended hours specific to after hour projects as needed)</li>
-                    <li><i class="fa fa-phone pr-2"></i> Prishtinë - 049885577</li>
-                    <li><i class="fa fa-envelope-o pr-2"></i>Lokacionet:Sheshi Nene Tereza rruga Hajdar Dushi</br>
-                        Bregu i Diellit: Perballe Maksi 24h</br>
-                        Ulpian  Perball Postes</br></li>
-                  </ul>
+               <div class=\"col-md-4 col-lg-4 ndryshe pb-5\">";
+               echo $mapper->getFooter(3)['text'];
+                 
+               echo
+                 " <ul>
+                    <li><i class=\"fa fa-clock-o pr-2\"></i>"; echo $mapper->getFooter(4)['text']."</li>
+                    <li><i class=\"fa fa-phone pr-2\"></i>";echo  $mapper->getFooter(5)['text']."</li>
+                    <li><i class=\"fa fa-envelope-o pr-2\"></i>"; echo $mapper->getFooter(6)['text'].
+                 " </ul>
                </div>
                
               
@@ -32,11 +34,11 @@
 
               
             </div>    
-            <div class="col-12 special">
-                <hr>
-                <p class="p1">Copyright © Maro Agency. 2020 | All Rights Reserved</p>
-                <p class="p2">Terms and Conditions | Privacy Policy</p>
-             </div>
+            <div class=\"col-12 special\">
+                <hr>";
+                echo $mapper->getFooter(7)['text'].
+             "</div>
           </div>      
         </div>
-     </footer>
+     </footer>";
+     ?>
