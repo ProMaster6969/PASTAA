e90a956ece9750f48a42f3b8516429f1255c76ba
berat422 <75537316+berat422@users.noreply.github.com>
2021-04-15 21:50:43 +0200

    echo

--- header.php
+++ header.php
@@ -1,44 +1,48 @@
 <?php
 session_start();
+
 ?>
- <header>
- <div class="container">
- <div id="branding">
-     <img src="Photos/logo.png" alt="">
+<?php
+echo 
+ "<header>
+ <div class=\"container\">
+ <div id=\"branding\">
+     <img src=\"Photos/logo.png\" >
   
  </div>
  <nav>
-   <ul>
+   <ul>";
+   ?>
   
   
    <?php
       if (isset($_SESSION['role']) && $_SESSION['role'] == 1) {
       ?>
-     <li class="current"> <a href="Dashboard.php"> Dashboard</a></li>
+      <?php
+      echo "<li class=\"current\"> <a href=\"Dashboard.php\"> Dashboard</a></li>";
+      ?>
       <?php
       }
       ?>
 
-
+<?php
   
-     <li class="current"><a href="Projekti.php ">Home</a></li>
-     <li class="current"><a href="services.php">Services</a></li>
-     <li class="current"><a href="aboutt.php">About</a></li>  
+    echo " <li class=\"current\"><a href=\"Projekti.php \">Home</a></li>".
+     "<li class=\"current\"><a href=\"services.php\">Services</a></li>
+     <li class=\"current\"><a href=\"aboutt.php\">About</a></li>
+     <li class=\"current\"><a href=\"product.php\">Product</a></li> ";
+     ?> 
      <?php
      if(isset($_SESSION['role'])&& $_SESSION['role']==0){
        ?>
-      <li class="current"><a href="ConatctUs.php">Contact Us</a></li>
+       <?php
+      echo "<li class=\"current\"><a href=\"ConatctUs.php\">Contact Us</a></li>";
+      ?>
       <?php
      }
      ?>  
     
-     <?php
-     if(!isset($_SESSION['role'])){
-       ?>
-      <li class="current"><a href="Register.php">Register</a></li>
-      <?php
-     }
-     ?>
+    
     
     
      <?php
