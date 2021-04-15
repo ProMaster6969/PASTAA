bffbfb9dd9c8eebfed5ba4905283331c7362c97d
ProMaster6969 <75331370+ProMaster6969@users.noreply.github.com>
2021-04-15 21:17:15 +0200

    Update Mapper.php
    funksione shtese ne mapper

--- Mapper.php
+++ Mapper.php
@@ -77,6 +77,66 @@ class Mapper extends DataBaseConfig
         $statement->bindParam(":id",$id);
         $statement->execute();
     }
+	public function getAlldata($id){
+        $this->query="select* from user where ID=:Id";
+        $statement=$this->connection->prepare($this->query);
+        $statement->bindParam(":Id",$id);
+        $statement->execute();
+        return $result=$statement->fetch(PDO::FETCH_ASSOC);
+
+
+    }
+public function update($id,$name,$email,$role){
+        $this->query="update user set name=:name, email=:email, role=:role
+        where ID=:id";
+        $statement=$this->connection->prepare($this->query);
+        $statement->bindParam(":id",$id);
+        $statement->bindParam(":name",$name);
+        $statement->bindParam(":email",$email);
+        $statement->bindParam(":role",$role);
+        $statement->execute();
+        
+
+
+    }
+    public function addProduct($foto,$emri,$qmimi){
+        $this->query="insert into productss (images,name,price) values (:foto,:name,:price)";
+        $statement=$this->connection->prepare($this->query);
+        $statement->bindParam(":foto",$foto);
+        $statement->bindParam(":name",$emri);
+        $statement->bindParam(":price",$qmimi);
+        $statement->execute();
+
+    }
+    public function getAllProducts(){
+        $this->query="select* from productss";
+        $statement=$this->connection->prepare($this->query);
+        $statement->execute();
+         return $resault=$statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function deleteproduct($ID){
+        $this->query="delete from productss where ID=:ID";
+        $statement=$this->connection->prepare($this->query);
+        $statement->bindParam(":ID",$ID);
+        $statement->execute();
+    }
+    public function getProductsbyId($id){
+        $this->query="select* from productss where ID=:id";
+        $statement=$this->connection->prepare($this->query);
+        $statement->bindParam(":id",$id);
+        $statement->execute();
+         return $resault=$statement->fetch(PDO::FETCH_ASSOC);
+    }
+    public function updateProduct($id,$foto,$emri,$qmimi){
+        $this->query="update  productss set images=:foto,name=:name,price=:price where ID=:id";
+        $statement=$this->connection->prepare($this->query);
+        $statement->bindParam(":id",$id);
+        $statement->bindParam(":foto",$foto);
+        $statement->bindParam(":name",$emri);
+        $statement->bindParam(":price",$qmimi);
+        $statement->execute();
+    
+}
     
 }
 
