07c6a1af6d9166c2950e3153ca798e7fb8cea200
berat422 <75537316+berat422@users.noreply.github.com>
2021-04-15 21:48:22 +0200

    334

--- services.php
+++ services.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-            <link rel="stylesheet" href="services.css" type="text/css"/>
+            <link rel="stylesheet" href="services.css?ts=<?=time()?>" type="text/css"/>
             <link rel="preconnect" href="https://fonts.gstatic.com">
             <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,600&display=swap" rel="stylesheet">
             <title>Services</title>
@@ -12,56 +12,55 @@
     <body>
     <?php
      include 'header.php';
+     require_once 'Mapper.php';
+     $mapper=new Mapper();
+
     ?>
-
-    <main>
-        <section class="hero"> 
-            <div class="bcontainer">
-                <div class="mesazhi-kryesor">
-                  <h1>Your Menu Sets the Mood</h1>
-                  <p>Remeber that your menu is part of</br>
-                  your cousine performance that offers</br>
-                  a colsumer not only quality food,but
-                  also quality time
-              </p>
+<?php
+    echo "<main>".
+      "  <section class=\"hero\"> ".
+            "<div class=\"bcontainer\">".
+               " <div class=\"mesazhi-kryesor\">".$mapper->getService(1)['text'];
+                 
+                 echo $mapper->getService(2)['text'].
+                 $mapper->getService(3)['text'].
+                 $mapper->getService(4)['text'];
             
-              <p>
-                Food is a very important in the life of each of us; many of us resort to dining in
-                 restaurants because of the delay outside the home 
-                or the need to break the routine of the daily life and taste new foods and flavors.
-              </p>
-                </div>
-            </div>
-        </section>
+              
+                echo"</div>".
+           " </div>".
+        "</section>".
           
-   <section class="heroi">
-    <div id="slider">
-         <figure>
+   "<section class=\"heroi\">".
+    "<div id=\"slider\">".
+         "<figure>".
           
-            <img src="Photos/222.jpg">
-            <img src="Photos/111.jpg">
-            <img src="Photos/333.jpg">
-            <img src="Photos/444.jpg">
-            <img src="Photos/555.jpg">
-         </figure>
-  </div>
+           " <img src=\"Photos/222.jpg\">".
+            "<img src=\"Photos/111.jpg\">".
+            "<img src=\"Photos/333.jpg\">".
+            "<img src=\"Photos/444.jpg\">".
+            "<img src=\"Photos/555.jpg\">".
+         "</figure>".
+  "</div>";
 
-</section>
-<section class="team">
-    <div class="teamDiv">
-        <div class="box1">
+ echo "</section>".
+"<section class=\"team\">".
+    "<div class=\"teamDiv\">".
+        "<div class=\"box1\">".
          
-          <img src="Photos/team.jpg" class="teamFoto">
+          "<img src=\"Photos/team.jpg\" class=\"teamFoto\">".
 
-        </div>
-        <div class="box2">
+        "</div>".
+        "<div class=\"box2\">";
          
-          <h2>Meet our Pasta Team</h2>
-          <p><b>"</b>I'm working harder than ever now, and I'm putting on my pants the same as I always have.</br> I just get up every day and try to do a little better than the day before, and that is to run a great restaurant with great food, great wine, and great service. That’s my philosophy.<b>"</b></p>
-        </div>
-    </div>
-</section>
-    </main>
+          echo $mapper->getService(5)['text'];
+          echo $mapper->getService(6)['text'];
+          echo $mapper->getService(7)['text'];
+        echo "</div>".
+    "</div>".
+"</section>".
+    "</main>";
+    ?>
     <?php
    include 'footer.php';
     ?>
